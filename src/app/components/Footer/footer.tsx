e6beb998d84210479f746991fc6cb877d7b62401
MatheusHenriqueNF <matheus.henrique26@unifesp.br>
2025-04-12 21:20:11 -0300

    Sobre | Adicionando responsividade

--- src/app/components/Footer/footer.tsx
+++ src/app/components/Footer/footer.tsx
@@ -3,17 +3,42 @@ import Link from "next/link";
 const Footer = () => {
     return (
         <>
-        <footer className='w-full bg-[#FEF2DC] text-[#2C2C25] font-light p-4 flex flex-col items-center justify-center'>
-            <h2 className='text-lg mb-4'>&copy; Todos os direitos reservados.</h2>
+            <footer className="w-full bg-[#FEF2DC] text-[#2C2C25] font-light p-6 flex flex-col items-center justify-center text-center">
+                <h2 className="text-base sm:text-lg mb-4">&copy; Todos os direitos reservados.</h2>
 
-            <div className='flex gap-6 text-lg text-center'>
-                <p><Link href='https://www.linkedin.com/in/cleyton-enrike-de-oliveira99/' target='_blank'><span className='font-semibold'>560485</span> Cleyton Enrike de Oliveira</Link></p>
-                <p><Link href='https://www.linkedin.com/in/matheus-henrique-freitas/' target='_blank'><span className='font-semibold'>560442</span> Matheus Henrique Nascimento</Link></p>
-                <p><Link href='https://www.linkedin.com/in/pedro-henrique-sena/' target='_blank'><span className='font-semibold'>561178</span> Pedro Henrique Sena</Link></p>
-            </div>
-        </footer>
+                <div className="flex flex-col sm:flex-row gap-4 sm:gap-6 text-sm sm:text-base text-center">
+                    <p>
+                    <Link href="https://www.linkedin.com/in/cleyton-enrike-de-oliveira99/" target="_blank">
+                        <span className="font-semibold">560485</span> Cleyton Enrike de Oliveira
+                    </Link>
+                    </p>
+                    <p>
+                    <Link href="https://www.linkedin.com/in/matheus-henrique-freitas/" target="_blank">
+                        <span className="font-semibold">560442</span> Matheus Henrique Nascimento
+                    </Link>
+                    </p>
+                    <p>
+                    <Link href="https://www.linkedin.com/in/pedro-henrique-sena/" target="_blank">
+                        <span className="font-semibold">561178</span> Pedro Henrique Sena
+                    </Link>
+                    </p>
+                </div>
+            </footer>
+
         </>
     )
 }
 
 export default Footer;
+
+
+
+// <footer className='w-full bg-[#FEF2DC] text-[#2C2C25] font-light p-4 flex flex-col items-center justify-center'>
+// <h2 className='text-lg mb-4'>&copy; Todos os direitos reservados.</h2>
+
+// <div className='flex gap-6 text-lg text-center'>
+//     <p><Link href='https://www.linkedin.com/in/cleyton-enrike-de-oliveira99/' target='_blank'><span className='font-semibold'>560485</span> Cleyton Enrike de Oliveira</Link></p>
+//     <p><Link href='https://www.linkedin.com/in/matheus-henrique-freitas/' target='_blank'><span className='font-semibold'>560442</span> Matheus Henrique Nascimento</Link></p>
+//     <p><Link href='https://www.linkedin.com/in/pedro-henrique-sena/' target='_blank'><span className='font-semibold'>561178</span> Pedro Henrique Sena</Link></p>
+// </div>
+// </footer>
